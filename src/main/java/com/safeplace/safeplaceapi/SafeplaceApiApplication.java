b64cbfae357676425e1c5f62df01256cba6d5965
Camila Perez <proggram.perezschami@outlook.com>
2026-03-21 16:48:28 -0300

    chore: initialize Spring Boot project and add health endpoint

---
+++ src/main/java/com/safeplace/safeplaceapi/SafeplaceApiApplication.java
@@ -0,0 +1,13 @@
+package com.safeplace.safeplaceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeplaceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SafeplaceApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/safeplace/safeplaceapi/SafeplaceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safeplace.safeplaceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeplaceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
